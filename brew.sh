7fc0d642e4ca5d9cc73d81c5545cf3430f468d3b
Giulio Starace <giulio.starace@gmail.com>
2020-11-29 17:30:54 +0100

    automate and track homebrew packages

---
+++ brew.sh
@@ -0,0 +1,27 @@
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update
+brew upgrade
+
+# Install packages
+fzf
+lf
+luarocks
+rbenv
+ripgrep
+tmux
+vim
+
+# Wait a bit before moving on...
+sleep 1
+
+# ...and then.
+echo "Success! Basic brew packages are installed."
+echo "Proceeding with brew cask packages..."
+
+# Install cask packages
+brew cask install middleclick
+brew cask install rectangle
+
+# the end
+echo "Success! Brew cask packages are installed."
