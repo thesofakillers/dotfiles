5ed6956c6b95e0a63c6bb16b86e4eadfcc6c7563
Giulio Starace <giulio.starace@gmail.com>
2020-11-29 17:35:14 +0100

    automate and track additional packages

---
+++ additional-installs.sh
@@ -0,0 +1,8 @@
+# additional installs
+echo "Installing additional packages. Make sure this is running towards the end
+of setup"
+
+# list install commands
+luarocks install digestif
+
+echo "Success! Installation of additional programs is complete."
