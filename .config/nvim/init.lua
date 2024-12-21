b642687d4c713d3174de5999522de576f46195d7
Giulio Starace <giulio.starace@gmail.com>
2024-12-21 15:36:14 +0100

    fix init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,4 @@
-vim.opt.runtimepath:prepend("~/.vim")
-vim.opt.runtimepath:append("~/.vim/after")
-vim.opt.packpath = vim.opt.runtimepath
-
 -- Load common vim settings
+vim.cmd('set runtimepath^=~/.vim runtimepath+=~/.vim/after')
+vim.o.packpath = vim.o.runtimepath
 vim.cmd('source ~/.vim/vimrc')
