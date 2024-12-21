1011f5b8f25cc096c30a3a8253386ecd6756793c
Giulio Starace <giulio.starace@gmail.com>
2024-12-21 16:03:20 +0100

    Revert "switch to init.lua"
    This reverts commit 1f5b1ab787650a5ce9dcf053ef0e374e907c4b40.

--- .config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-vim.opt.runtimepath:prepend("~/.vim")
-vim.opt.runtimepath:append("~/.vim/after")
-vim.opt.packpath = vim.opt.runtimepath
-
--- Load common vim settings
-vim.cmd('source ~/.vim/vimrc')
