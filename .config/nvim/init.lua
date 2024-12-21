d9b48779e3aa982956f74205060afc6c7f5e482a
Giulio Starace <giulio.starace@gmail.com>
2024-12-21 20:52:48 +0100

    install avante

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,21 @@
 vim.cmd('set runtimepath^=~/.vim runtimepath+=~/.vim/after')
 vim.o.packpath = vim.o.runtimepath
 vim.cmd('source ~/.vim/vimrc')
+
+
+require("copilot").setup({
+  suggestion = {
+    auto_trigger = true,
+    keymap = {
+      accept = "<C-l>",
+    }
+  }
+})
+
+require('avante_lib').load()
+require('avante').setup({
+  auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+  behaviour = {
+    auto_apply_diff_after_generation = true,
+  }
+})
