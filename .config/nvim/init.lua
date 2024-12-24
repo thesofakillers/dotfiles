074afe652ea03392b71f25355a1e2c7539ae6177
Giulio Starace <giulio.starace@gmail.com>
2024-12-24 18:10:03 +0100

    remove conflicts with vanilla vim c mappings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,5 +18,17 @@ require('avante').setup({
   auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
   behaviour = {
     auto_apply_diff_after_generation = true,
+  },
+  mappings = {
+    ---@class AvanteConflictMappings
+    diff = {
+      ours = "<leader>co",
+      theirs = "<leader>ct",
+      all_theirs = "<leader>ca",
+      both = "<leader>cb",
+      cursor = "<leader>cc",
+      next = "]x",
+      prev = "[x",
+    }
   }
 })
