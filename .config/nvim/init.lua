f56d1444eab2d8bf30bfaf0c77e2f7417ebd5ce4
Giulio Starace <giulio.starace@gmail.com>
2025-05-12 21:11:14 +0200

    get rid of avante temporarily

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,44 +16,44 @@ require("copilot").setup({
   }
 })
 
-require('avante').setup({
-  auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
-  behaviour = {
-    auto_apply_diff_after_generation = true,
-  },
-  mappings = {
-    ---@class AvanteConflictMappings
-    diff = {
-      ours = "<leader>co",
-      theirs = "<leader>ct",
-      all_theirs = "<leader>ca",
-      both = "<leader>cb",
-      cursor = "<leader>cu",
-      next = "]x",
-      prev = "[x",
-    },
-    files = {
-      add_current = "<leader>ab" -- Add current buffer to selected files
-    }
-  }
-})
--- Treat Avante files as markdown
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "Avante",
-  callback = function()
-    vim.bo.filetype = "markdown"
-  end,
-})
+--require('avante').setup({
+--  auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+--  behaviour = {
+--    auto_apply_diff_after_generation = true,
+--  },
+--  mappings = {
+--    ---@class AvanteConflictMappings
+--    diff = {
+--      ours = "<leader>co",
+--      theirs = "<leader>ct",
+--      all_theirs = "<leader>ca",
+--      both = "<leader>cb",
+--      cursor = "<leader>cu",
+--      next = "]x",
+--      prev = "[x",
+--    },
+--    files = {
+--      add_current = "<leader>ab" -- Add current buffer to selected files
+--    }
+--  }
+--})
+---- Treat Avante files as markdown
+--vim.api.nvim_create_autocmd({ "FileType" }, {
+--  pattern = "Avante",
+--  callback = function()
+--    vim.bo.filetype = "markdown"
+--  end,
+--})
 
--- Disable hard line breaks for AvanteInput files
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "AvanteInput",
-  callback = function()
-    vim.bo.textwidth = 0
-  end,
-})
+---- Disable hard line breaks for AvanteInput files
+--vim.api.nvim_create_autocmd({ "FileType" }, {
+--  pattern = "AvanteInput",
+--  callback = function()
+--    vim.bo.textwidth = 0
+--  end,
+--})
 
 
 
--- leader mappings
-vim.keymap.set("n", "<leader>ac", ":AvanteClear<CR>")
+---- leader mappings
+--vim.keymap.set("n", "<leader>ac", ":AvanteClear<CR>")
